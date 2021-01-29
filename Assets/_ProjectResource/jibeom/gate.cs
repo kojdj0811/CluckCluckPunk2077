dc0b97bd7245059fd92b7ef9c1a749da57c89b81
Park <mayer0425@naver.com>
2021-01-30 05:43:54 +0900

    좌우, 상하, 흔들림

---
+++ Assets/_ProjectResource/jibeom/gate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gate : MonoBehaviour
+{
+    public GameObject Cam;
+    public string direction;
+    Vector3 dir;
+    cameraMove cam;
+
+    void Start()
+    {
+        cam = Cam.GetComponent<cameraMove>();
+        if (direction == "right")
+            dir = Vector3.right;
+        else if (direction == "left")
+            dir = Vector3.left;
+        else if (direction == "up")
+            dir = Vector3.up;
+        else if (direction == "down")
+            dir = Vector3.down;
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+                        cam.StartCoroutine(cam.move(dir));
+            //            cam.StartCoroutine(cam.shakeLeftRight());
+//            cam.StartCoroutine(cam.shakeUpDown());
+
+        }
+    }
+}
