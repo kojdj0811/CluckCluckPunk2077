ee427c119c042535966a6751d8cb03eeffd80bdb
kojdj0811 <kojdj0811@gmail.com>
2021-01-30 00:42:56 +0900

    init

---
+++ Assets/_ProjectResource/EventManager/EventManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public interface IEvent{}
+
+
+#region SampleEvnet
+public struct PrintLogEvent : IEvent 
+{
+    public string message;
+    public PrintLogEvent(string message)
+    {
+        this.message = message;
+    }
+}
+#endregion //SampleEvnet
+
+
+
+
+
+#region Events
+
+
+#endregion //Events
+
+
+
+
+
+
+
+public class EventHandler: UnityEvent<IEvent> {}
+
+public class EventManager
+{
+    static private Dictionary<Type, EventHandler> eventDictionary = new Dictionary<Type, EventHandler>();
+
+    static public void StartListening(Type type, UnityAction<IEvent> listener)
+    {
+        EventHandler handler = null;
+        if (eventDictionary.TryGetValue(type, out handler))
+        {
+            handler.AddListener(listener);
+        }
+        else
+        {
+            handler = new EventHandler();
+            handler.AddListener(listener);
+            eventDictionary.Add(type, handler);
+        }            
+    }
+
+    static public void StopListening(Type type, UnityAction<IEvent> listener)
+    {
+        EventHandler handler = null;
+        if (eventDictionary.TryGetValue(type, out handler))
+        {
+            handler.RemoveListener(listener);
+        }
+    }
+
+    static public void TriggerEvent(IEvent eventObject)
+    {
+        EventHandler handler = null;
+        if (eventDictionary.TryGetValue(eventObject.GetType(), out handler))
+        {
+            handler.Invoke(eventObject);
+        }
+    }
+}
