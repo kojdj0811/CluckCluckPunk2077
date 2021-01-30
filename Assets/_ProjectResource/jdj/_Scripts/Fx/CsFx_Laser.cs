2e3692dc9dc60e66560c77c95df5130f4e54a693
kojdj0811 <kojdj0811@gmail.com>
2021-01-31 00:04:47 +0900

    레이저 만드는중...

---
+++ Assets/_ProjectResource/jdj/_Scripts/Fx/CsFx_Laser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsFx_Laser : CsFxEntity
+{
+    public Transform raserAnchor;
+    public LayerMask targetLayer;
+
+    private Vector3 from;
+    private Vector3 to;
+
+    public float rayRange = 50.0f;
+
+    public override void Awake() {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void Start() {
+        from = transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(from, transform.up, rayRange, targetLayer);
+
+        if(hit)
+            to = hit.point;
+        else
+            to = transform.position + transform.up * rayRange;
+
+
+
+        
+
+    }
+}
