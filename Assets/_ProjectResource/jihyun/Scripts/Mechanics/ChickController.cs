7a8d3eced746f2173ebef7d58238f0efd17a8688
jihyun <bgm.leejihyun@gmail.com>
2021-01-30 11:09:04 +0700

    add  - jihyun 폴더로 작업 업데이트  - 플랫포머 예제 코드 적용해서 플레이어 적용

---
+++ Assets/_ProjectResource/jihyun/Scripts/Mechanics/ChickController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// A simple controller for enemies. Provides movement control over a patrol path.
+    /// </summary>
+    [RequireComponent(typeof(AnimationController), typeof(Collider2D))]
+    public class ChickController : MonoBehaviour
+    {
+        public AudioClip ouch;
+
+        internal AnimationController control;
+        internal Collider2D _collider;
+        internal AudioSource _audio;
+        SpriteRenderer spriteRenderer;
+
+        public Bounds Bounds => _collider.bounds;
+
+        void Awake()
+        {
+            control = GetComponent<AnimationController>();
+            _collider = GetComponent<Collider2D>();
+            _audio = GetComponent<AudioSource>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                var ev = Schedule<PlayerEnemyCollision>();
+                ev.player = player;
+            }
+        }
+
+        void Update()
+        {
+        }
+
+    }
+}
