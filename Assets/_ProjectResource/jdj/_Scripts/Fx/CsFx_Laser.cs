aadb6cd66d0c5eec09491e740efb3de3d40f57cb
kojdj0811 <kojdj0811@gmail.com>
2021-01-31 08:30:01 +0900

    Revert "Stage1 라이트 세팅"
    This reverts commit d50ef6baedbef705eafc50290dc1b1f4412fa128.

--- Assets/_ProjectResource/jdj/_Scripts/Fx/CsFx_Laser.cs
+++ Assets/_ProjectResource/jdj/_Scripts/Fx/CsFx_Laser.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class CsFx_Laser : CsFxEntity
 {
     public Transform raserAnchor;
+    public SpriteRenderer spriteRenderer;
     public LayerMask targetLayer;
+
+    public AnimationCurve fadeoutCurve;
 
     private Vector3 from;
     private Vector3 to;
 
-    public float rayRange = 50.0f;
+    public float rayRange = 30.0f;
+    public float maxLength;
+    public float thickness = 0.47f;
+
+
+
+    private Color initColor;
+    private float animStartTime;
 
     public override void Awake() {
         Destroy(gameObject, lifetime);
+        animStartTime = Time.timeSinceLevelLoad;
+        initColor = spriteRenderer.color;
     }
+
+
 
     private void Start() {
         from = transform.position;
@@ -28,7 +42,15 @@ public class CsFx_Laser : CsFxEntity
 
 
 
-        
+        float dist = Vector3.Distance(from, to);
+        raserAnchor.localScale = new Vector3(thickness, maxLength * dist / rayRange, 1.0f);
+    }
 
+    private void Update() {
+        Color color = initColor;
+        float u = (Time.timeSinceLevelLoad - animStartTime) / lifetime;
+        color.a = fadeoutCurve.Evaluate(u);
+
+        spriteRenderer.color = color;
     }
 }
